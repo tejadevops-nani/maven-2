03d09e729b7f1290808d57baa60d5df6d38df64e
root <root@ip-172-31-1-235.ap-south-1.compute.internal>
2024-10-18 11:07:35 +0000

    maven

---
+++ homepage/src/main/java/Tiktak/Demo.java
@@ -0,0 +1,13 @@
+package Tiktak;
+
+/**
+ * Hello world!
+ *
+ */
+public class Demo
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World12345!" );
+    }
+}
